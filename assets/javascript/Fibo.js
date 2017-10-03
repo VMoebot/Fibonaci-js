6d91501a9a25c13bc7f052d4057be464e5794133
Jose Alfredo P.C <32446743+VMoebot@users.noreply.github.com>
2017-10-02 22:54:37 -0500

    Fibo N en terminos

--- assets/javascript/Fibo.js
+++ assets/javascript/Fibo.js
@@ -1,13 +1,14 @@
 function fibo()
 {
+ var n=Number(prompt("dame un numero "))
   Vfibo= new Array(10);
   Vfibo[0]=0;
   Vfibo[1]=1;
-  for(c=2; c<10; c++)
+  for(c=2; c<n; c++)
   {
     Vfibo[c]=Vfibo[c-1]+Vfibo[c-2];
   }
-  for(c=0; c<10; c++)
+  for(c=0; c<n; c++)
   {
     console.log(Vfibo[c]);
     document.write(Vfibo[c]);
